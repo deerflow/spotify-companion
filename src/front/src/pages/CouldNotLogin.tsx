b86c36aef088d8ab15621cf34fa752e0b90b97b0
Florian <alu.florian@gmail.com>
2023-02-06 14:07:21 +0100

    fix: Change Link to a in CouldNotLogin component

--- src/front/src/pages/CouldNotLogin.tsx
+++ src/front/src/pages/CouldNotLogin.tsx
@@ -1,14 +1,13 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
 import PageCenter from '../components/PageCenter';
 
 const CouldNotLogin: FC = () => {
     return (
         <PageCenter>
             <h1 style={styles}>Could not login to Spotify</h1>
-            <Link to='/users/login' className='spotify-login-button'>
+            <a href='/users/login' className='spotify-login-button'>
                 Try again
-            </Link>
+            </a>
         </PageCenter>
     );
 };
